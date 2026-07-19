a3bfc8c3cee85b9d01507594bff217ce2aac580b
2022sp031-lang <2022sp031@univ.jfn.ac.lk>
2026-07-19 14:13:23 +0530

    Modified a function ✏️

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,7 +59,8 @@ export async function loginUser(req, res) {
 
                 res.json(
                     {
-                        token: token
+                        token: token,
+                        isAdmin: user.isAdmin
                     }
                 )
 
