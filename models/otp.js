877bf56114fc3ce7264e44cb1b02938fb9860e2d
2022sp031-lang <2022sp031@univ.jfn.ac.lk>
2026-09-20 00:51:28 +0530

    Added a new model ✏️

---
+++ models/otp.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema(
+    {
+        email : {
+            type : String,
+            required : true,
+            unique : true
+        },
+
+        otp : {
+            type : String,
+            required : true
+        },
+
+        createTime : {
+            type : Date,
+            required : true,
+            default : Date.now
+        }
+    }
+);
+
+const OTP = mongoose.model("OTP", otpSchema);
+export default OTP
